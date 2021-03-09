b3aea086de0044088e12dfc18a2c4769d7da63d3
augustin desalos <augustin.desalos@gmail.com>
2021-03-09 18:56:15 +0100

    many modif at routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :fakes, only: [:index, :show, :new]
+  resources :fakes, only: [:show, :new] do
+    collection do
+      get "/category/:category", to: "fakes#index"
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
