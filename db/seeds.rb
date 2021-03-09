9fec765b2e53ba8bb763265337faefe8f5a3b293
eric nguyen <grider.uleched13@gmail.com>
2021-03-09 14:49:36 +0100

    seedfinish

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,14 @@ user1 = User.new(email: "toto@gmail.com",password: "toto21")
 user1.save
 user = User.last
 puts "creation de personnalité"
-angry_boss = {user_id: user,nickname: "sylvain", url: "photo", category: "boss", personality: "angry", description: "?", price: 150, rating: 5}
-crazy_buddy = {user_id: user,nickname: "jean-loup", url: "photo", category: "buddy", personality: "crazy", description: "?", price: 12, rating: 4}
-romantic_dinner = {user_id: user,nickname: "julien", url: "photo", category: "dinner", personality: "romantic", description: "?", price: 34, rating: 5}
-bodyguard = {user_id: user,nickname: "théo", url: "photo", category: "guard", personality: "bodyguard", description: "?", price: 500, rating: 5}
-brother = {user_id: user,nickname: "stéphane", url: "photo", category: "brother", personality: "reuf", description: "?", price: 200, rating: 4}
-nice_boss = {user_id: user,nickname: "mathilde", url: "photo", category: "boss", personality: "excellent", description: "?", price: 300, rating: 4}
+angry_boss = {user_id: user.id,nickname: "sylvain", url: "photo", category: "boss", personality: "angry", description: "?", price: 150, rating: 5}
+crazy_buddy = {user_id: user.id,nickname: "jean-loup", url: "photo", category: "buddy", personality: "crazy", description: "?", price: 12, rating: 4}
+romantic_dinner = {user_id: user.id,nickname: "julien", url: "photo", category: "dinner", personality: "romantic", description: "?", price: 34, rating: 5}
+bodyguard = {user_id: user.id,nickname: "théo", url: "photo", category: "guard", personality: "bodyguard", description: "?", price: 500, rating: 5}
+brother = {user_id: user.id,nickname: "stéphane", url: "photo", category: "brother", personality: "reuf", description: "?", price: 200, rating: 4}
+nice_boss = {user_id: user.id,nickname: "mathilde", url: "photo", category: "boss", personality: "excellent", description: "?", price: 300, rating: 4}
 [ angry_boss, crazy_buddy,romantic_dinner,bodyguard,brother, nice_boss ].each do |attributes|
   fake = Fake.create!(attributes)
-  puts "Created #{fake.name}"
+  puts "Created #{fake.nickname}"
 end
 puts "Finished!"
