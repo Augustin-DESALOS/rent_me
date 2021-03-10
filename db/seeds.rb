a4e6799802c535d5f19a4d0718966b67deba0c04
augustin desalos <augustin.desalos@gmail.com>
2021-03-10 10:34:38 +0100

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,16 @@ user1 = User.new(email: "toto@gmail.com",password: "toto21")
 user1.save
 user = User.last
 puts "creation de personnalité"
-boss1 = {user_id: user.id,nickname: "sylvain", url: "sylvain.jpeg", category: "boss", personality: "angry2.png", description: "lorem", price: 150, rating: 5}
-boss2 = {user_id: user.id,nickname: "mathilde", url: "photo", category: "boss", personality: "noisy.png", description: "lorem", price: 300, rating: 4}
-boss3 = {user_id: user.id,nickname: "bruno", url: "photo", category: "boss", personality: "authoritarian.jpg", description: "lorem", price: 35, rating: 4}
-boss4 = {user_id: user.id,nickname: "pauline", url: "photo", category: "boss", personality: "angry2.png", description: "lorem", price: 250, rating: 4}
-friend1 = {user_id: user.id,nickname: "jean-loup", url: "photo", category: "friend", personality: "noisy.png", description: "lorem", price: 12, rating: 4}
-friend2 = {user_id: user.id,nickname: "julien", url: "photo", category: "friend", personality: "nice.jpg", description: "lorem", price: 89, rating: 5}
-friend3 = {user_id: user.id,nickname: "remy", url: "photo", category: "friend", personality: "authoritarian.jpg", description: "lorem", price: 218, rating: 5}
-friend4 = {user_id: user.id,nickname: "damien", url: "photo", category: "friend", personality: "nice.jpg", description: "lorem", price: 99, rating: 5}
-family = {user_id: user.id,nickname: "théo", url: "photo", category: "family", personality: "noisy.png", description: "lorem", price: 500, rating: 5}
-employee = {user_id: user.id,nickname: "stéphane", url: "photo", category: "employee", personality: "angry2.png", description: "lorem", price: 200, rating: 4}
+boss1 = {user_id: user.id,nickname: "sylvain", url: "sylvain.jpeg", category: "boss", personality: "angry", description: "lorem", price: 150, rating: 5}
+boss2 = {user_id: user.id,nickname: "mathilde", url: "photo", category: "boss", personality: "noisy", description: "lorem", price: 300, rating: 4}
+boss3 = {user_id: user.id,nickname: "bruno", url: "photo", category: "boss", personality: "authoritarian", description: "lorem", price: 35, rating: 4}
+boss4 = {user_id: user.id,nickname: "pauline", url: "photo", category: "boss", personality: "angry", description: "lorem", price: 250, rating: 4}
+friend1 = {user_id: user.id,nickname: "jean-loup", url: "photo", category: "friend", personality: "noisy", description: "lorem", price: 12, rating: 4}
+friend2 = {user_id: user.id,nickname: "julien", url: "photo", category: "friend", personality: "nice", description: "lorem", price: 89, rating: 5}
+friend3 = {user_id: user.id,nickname: "remy", url: "photo", category: "friend", personality: "authoritarian", description: "lorem", price: 218, rating: 5}
+friend4 = {user_id: user.id,nickname: "damien", url: "photo", category: "friend", personality: "nice", description: "lorem", price: 99, rating: 5}
+family = {user_id: user.id,nickname: "théo", url: "photo", category: "family", personality: "noisy", description: "lorem", price: 500, rating: 5}
+employee = {user_id: user.id,nickname: "stéphane", url: "photo", category: "boss", personality: "angry", description: "lorem", price: 200, rating: 4}
 
 [ boss1, boss2,boss3,boss4, friend1, friend2, friend3, friend4, family, employee ].each do |attributes|
   fake = Fake.create!(attributes)
