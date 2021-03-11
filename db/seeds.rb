e892f0af4b7f33517cee084a9d1b4907db0dfb1c
augustin desalos <augustin.desalos@gmail.com>
2021-03-11 18:14:40 +0100

    wesh

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Cleaning database..."
+Renting.destroy_all
 Fake.destroy_all
 User.destroy_all
 puts "creation d'utilisateurs"
